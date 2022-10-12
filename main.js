9dd7d4156836edbcba27d256fa9b0549c22a6943
thisguyse19 <115332134+thisguyse19@users.noreply.github.com>
2022-10-12 21:49:04 +0800

    Edit

--- main.js
+++ main.js
@@ -21,6 +21,12 @@ function lightmode() {
   buy1.classList.add("greyish","bglight");
 }}
 
+function superToggle (elementvar,class0,class1) {
+  var ele = elementvar
+  ele.classList.toggle(class0);
+  ele.classList.toggle(class1);
+}
+
 function goMain() {
   var sweat = document.getElementById("buy1").value;
   if (sweat == "sweat sweat") {
@@ -34,34 +40,11 @@ function goMain() {
   }
 }
 
-// Get the modal
-var modal = document.getElementById("add-item-modal");
-let varno = 1
-let totalpx = 0
-
-// Get the button that opens the modal
-var openbtn = document.getElementById("add-item");
-
-// Get the <span> element that closes the modal
-var closebtn = document.getElementsByClassName("closebtn")[0];
-
-var addbtn = document.getElementsByClassName("add-cfm")[0];
-
-// When the user clicks on the button, open the modal 
-openbtn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-closebtn.onclick = function() {
-  modal.style.display = "none";
-
-}
 
 function addItemSubmit() {
   
   const itemdisplay = document.getElementById("item-display-block");
-  let itemhtml = `<div id='item-display-indiv-${varno}' class='item-display-indiv'>
+  let itemhtml = `<div id='item-display-indiv-${varno}' onclick='openRetrieveModal(${varno})' class='item-display-indiv'>
   <div id="item-desc" class="item-name-vert">
     <p class="disp-name" id='show-itemName-${varno}'></p>
     <p class="disp-name-under" id="show-itemNotes-${varno}"></p>
@@ -93,11 +76,11 @@ function addItemSubmit() {
   varno += 1;
   totalpx += parseFloat(itemPrice.value);
   document.getElementById(`totalPrice`).innerHTML = "$ " + totalpx;
-  document.getElementById('header-title').innerHTML = "&#128722;  Your cart items.";
+  document.getElementById('header-title').innerHTML = "&#128722;  Shopping list";
   // document.getElementsByClassName("item-name")[0].innerHTML = " "
   // itemPrice.innerHTML = " "
   // itemNotes.innerHTML = " "
-  modal.style.display = "none";
+  superToggle(modal,"is-show-none","is-show-none-trans");
 }
 
 var itemName = document.getElementsByClassName("item-name")[0];
@@ -121,7 +104,51 @@ itemNotes.addEventListener("keypress", function(event) {
 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    superToggle(modal,"is-show-none","is-show-none-trans");
   }
 }
 
+
+
+// Modal for new item
+var modal = document.getElementById("add-item-modal");
+let varno = 1
+let totalpx = 0
+
+var openbtn = document.getElementById("add-item");
+
+var closebtn = document.getElementsByClassName("closebtn")[0];
+
+var addbtn = document.getElementsByClassName("add-cfm")[0];
+
+openbtn.onclick = function() {
+  superToggle(modal,"is-show-none","is-show-none-trans");
+}
+
+closebtn.onclick = function() {
+  superToggle(modal,"is-show-none","is-show-none-trans");
+
+}
+
+// Modal for existing item
+var retrieveModal = document.getElementById("retrieve-item-modal");
+
+var closeRetrieveBtn = document.getElementsByClassName("closeRbtn")[0];
+
+
+function openRetrieveModal(divno) {
+  superToggle(retrieveModal,"is-show-none","is-show-none-trans");
+  document.getElementById(`item-modal-name`).innerHTML = document.getElementById(`show-itemName-`+ divno).innerHTML;
+  document.getElementById(`item-modal-notes`).innerHTML = document.getElementById(`show-itemNotes-`+ divno).innerHTML;
+  document.getElementById(`item-modal-price`).innerHTML = document.getElementById(`show-itemPrice-`+ divno).innerHTML;
+}
+
+closeRetrieveBtn.onclick = function() {
+  superToggle(retrieveModal,"is-show-none","is-show-none-trans");
+}
+
+window.onclick = function(event) {
+  if (event.target == retrieveModal) {
+    superToggle(retrieveModal,"is-show-none","is-show-none-trans");
+  }
+}
